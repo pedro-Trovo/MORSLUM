d6cc815cf4a28ac21f272a37123fe0236331d420
Pedro Trovo <trovopedro5@gmail.com>
2026-02-17 11:44:22 -0300

    feat: componente QuizHeader para cabeçalho do quiz

---
+++ front-end/src/components/quiz/QuizHeader.jsx
@@ -0,0 +1,7 @@
+export default function QuizHeader() {
+  return (
+    <div className="text-center mb-3 md:mb-2">
+      <p className="text-color-secondary m-0">Selecione a alternativa correta</p>
+    </div>
+  );
+}
