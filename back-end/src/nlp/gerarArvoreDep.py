95db0a89cdf8c6df2910571d895533e1ff4cb2bf
Pedro Trovo <trovopedro5@gmail.com>
2026-02-16 23:51:46 -0300

    style(nlp): atualiza formatacao do display da arvore

--- back-end/src/nlp/gerarArvoreDep.py
+++ back-end/src/nlp/gerarArvoreDep.py
@@ -9,16 +9,16 @@ def gerar_arvore(frase):
 
     options = {
         "compact": False,
-        "distance": 150,
+        "distance": 85,
         "bg": "#072912",
         "color": "#fffcfc",
         "font": "Arial",
         "arrow_stroke": 1,
         "arrow_width": 6,
-        "word_spacing": 35,
-        "arrow_spacing": 40,
+        "word_spacing": 25,
+        "arrow_spacing": 35,
         "add_lemma": False,
-        "collapse_punct": False,
+        "collapse_punct": True,
         "collapse_phrases": False,
         "fine_grained": False,
         "offset_x": 90
