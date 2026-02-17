3a3089df91ad42cd55279624763275f150f19094
Pedro Trovo <trovopedro5@gmail.com>
2026-02-17 11:44:22 -0300

    feat: componente QuizFooter com navegação/controles

---
+++ front-end/src/components/quiz/QuizFooter.jsx
@@ -0,0 +1,10 @@
+export default function QuizFooter() {
+  return (
+    <div className="text-center mt-1 text-color-secondary text-sm">
+      <p className="m-0">
+        <i className="pi pi-info-circle mr-2"></i>
+        Selecione uma alternativa e clique em "Responder" para verificar sua resposta
+      </p>
+    </div>
+  );
+}
