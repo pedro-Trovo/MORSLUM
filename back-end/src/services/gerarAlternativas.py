db94941b7ddd0ab4bb99f8ff3aa6bbfdd3886c8e
Pedro Trovo <trovopedro5@gmail.com>
2026-02-24 21:18:54 -0300

    Refactor: refatora geração de alternativas para aumentar complexidade do quiz

--- back-end/src/services/gerarAlternativas.py
+++ back-end/src/services/gerarAlternativas.py
@@ -2,24 +2,29 @@ import random
 from src.nlp.classificarFrase import classificar_frase
 from src.utils.tradutor_tags import traduzir_tags
 
+
 def gerarAlternativas(frase):
     correta = classificar_frase(frase)
     correta_traduzida = traduzir_tags(correta)
 
-    alternativas = []
-
-
-    alternativas.append(correta_traduzida)
-
+    alternativas = [correta_traduzida]
 
     while len(alternativas) < 4:
-        alternativa = correta.copy()
-        random.shuffle(alternativa)
-        alternativa_traduzida = traduzir_tags(alternativa)
+        alternativa_raw = correta.copy()
 
-        if alternativa_traduzida not in alternativas:
+
+        if random.random() < 0.7:
+            i, j = random.sample(range(len(alternativa_raw)), 2)
+            alternativa_raw[i], alternativa_raw[j] = alternativa_raw[j], alternativa_raw[i]
+        else:  
+            for _ in range(2):
+                i, j = random.sample(range(len(alternativa_raw)), 2)
+                alternativa_raw[i], alternativa_raw[j] = alternativa_raw[j], alternativa_raw[i]
+
+        alternativa_traduzida = traduzir_tags(alternativa_raw)
+
+        if alternativa_traduzida != correta_traduzida and alternativa_traduzida not in alternativas:
             alternativas.append(alternativa_traduzida)
 
     random.shuffle(alternativas)
-
     return correta_traduzida, alternativas
