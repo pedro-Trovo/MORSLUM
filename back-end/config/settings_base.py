5c30033561249c1a590eaf4c3ba99e6b300ad234
Pedro Trovo <trovopedro5@gmail.com>
2026-03-08 01:35:38 -0300

    chore: atualiza .gitignore para receber estrutura electron

--- back-end/config/settings_base.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-
-class BaseConfig:
-    SESSION_PERMANENT = True
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
-    SESSION_USE_SIGNER = True
-    SESSION_KEY_PREFIX = "ic_session:"
-
-    CORS_SUPPORTS_CREDENTIALS = True
--- back-end/config/settings_dev.py
+++
@@ -1,7 +0,0 @@
-import os
-from redis import Redis
-from .settings_base import BaseConfig
-
-class DevConfig(BaseConfig):
-    SESSION_TYPE = "redis"
-    SESSION_REDIS = Redis.from_url(os.getenv("REDIS_URL"))
