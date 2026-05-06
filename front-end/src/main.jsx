25e0246b1799c1da625229c077e0b68efb4689a3
Pedro Trovo <trovopedro5@gmail.com>
2026-05-05 23:40:52 -0300

    feat: altera theme do projeto

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "primereact/resources/themes/vela-blue/theme.css";
+import "primereact/resources/themes/lara-dark-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
